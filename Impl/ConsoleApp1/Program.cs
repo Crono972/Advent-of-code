a1e5fa3c69d7ce8c5f41227c115215824d010e25
Fred Hocs <crono972@gmail.com>
2022-12-07 00:00:33 +0100

    2022 Exo06-01

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,67 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text.RegularExpressions;
+var str = File.ReadAllLines(@"../../../../../2022/Exo06/input.txt");
 
-var str = File.ReadAllLines(@"../../../../../2022/Exo05/input.txt");
-
-var towers = new List<Stack<char>>();
-
-var position = str[8];
-for (int i = 0; i < position.Length; i++)
+var line = str.Single();
+for (int i = 0; i < line.Length; i++)
 {
-    var currentChar = position[i];
-    if (Char.GetNumericValue(currentChar) != -1.0d)
+    var a = line[i];
+    var b = line[i + 1];
+    var c = line[i + 2];
+    var d = line[i + 3];
+    var hashSet = new HashSet<char>();
+    hashSet.Add(a);
+    hashSet.Add(b);
+    hashSet.Add(c);
+    hashSet.Add(d);
+    if (hashSet.Count == 4)
     {
-        var stack = new Stack<char>();
-        int j = 7;
-        var towerChar = str[j][i];
-        while (j >= 0 && towerChar != ' ')
-        {
-            stack.Push(towerChar);
-            j--;
-            if (j >= 0)
-            {
-                towerChar = str[j][i];
-            }
-        }
-        towers.Add(stack);
+        Console.WriteLine(i + 4);
+        break;
     }
 }
-
-foreach (var movement in str.Skip(10))
-{
-    if (!string.IsNullOrEmpty(movement))
-    {
-        var instr = Regex.Match(movement, @"move (\d+) from (\d+) to (\d+)");
-        var qty = int.Parse(instr.Groups[1].Value);
-        var from = int.Parse(instr.Groups[2].Value);
-        var toward = int.Parse(instr.Groups[3].Value);
-        Move(towers, qty, from, toward);
-    }
-}
-
-
-var result = string.Empty;
-foreach (var tower in towers)
-{
-    result += tower.Peek();
-}
-
-Console.WriteLine(result);
-
-void Move(List<Stack<char>> towers, int qty, int from, int to)
-{
-    var fromTower = towers[from - 1];
-    var toTower = towers[to - 1];
-    var localStack = new Stack<char>();
-    for (int i = 0; i < qty; i++)
-    {
-        var elt = fromTower.Pop();
-        localStack.Push(elt);
-    }
-
-    while (localStack.TryPop(out var elt))
-    {
-        toTower.Push(elt);
-    }
-}
+Console.ReadKey();
