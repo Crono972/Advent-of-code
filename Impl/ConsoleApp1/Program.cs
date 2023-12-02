38e5375fa2a7f2b7ea36e0de3f8e850ed23d7d7f
Fred Hocs <crono972@gmail.com>
2023-12-02 21:05:52 +0100

    2023 - Exo 01 - Part 02

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -15,14 +15,45 @@ public class Program
 
         int sum = 0;
 
+        var args = new Dictionary<string, char>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "one", '1' },
+            { "two", '2' },
+            { "three", '3' },
+            { "four", '4' },
+            { "five", '5' },
+            { "six", '6' },
+            { "seven", '7' },
+            { "eight", '8' },
+            { "nine", '9' }
+        };
+
+        var argsByLenghts = args.ToLookup(a => a.Key.Length);
+
         foreach (var line in lines)
         {
             var calibrationValue = string.Empty;
             char lastValue = 'a';
 
-            foreach (var character in line)
+            for (int i = 0; i < line.Length; i++)
             {
-                if(Char.IsNumber(character))
+                var character = line[i];
+                foreach (var arg in argsByLenghts)
+                {
+                    var left = line.Substring(i, Math.Min(line.Length - i, arg.Key));
+                    if (left.Length == arg.Key)
+                    {
+                        foreach (var pair in arg)
+                        {
+                            if (left.Equals(pair.Key))
+                            {
+                                character = pair.Value;
+                            }
+                        }
+                    }
+                }
+
+                if (char.IsNumber(character))
                 {
                     if (lastValue == 'a')
                     {
@@ -30,6 +61,7 @@ public class Program
                     }
                     lastValue = character;
                 }
+
             }
 
             calibrationValue += lastValue;
@@ -38,6 +70,5 @@ public class Program
 
         Console.WriteLine(sum);
     }
-
 
 }
