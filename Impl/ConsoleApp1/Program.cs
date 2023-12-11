48b7403c7406132e8d9e0acff0bede23b64a629d
FHocs <crono972@gmail.com>
2023-12-10 23:17:20 -0400

    2023 - Exo 04 Part 2

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -13,7 +13,7 @@ public class Program
     public static void Solve()
     {
         var lines = File.ReadAllLines(@"../../../../../2023/Exo04/input.txt");
-
+        var scratchCard = Enumerable.Repeat(1, lines.Length).ToArray();
         double part1 = 0;
         var regex = new Regex("Card\\s+(?<CardNumber>\\d+):(?:\\s+(?<WinningNumber>\\d+))+ \\|(?:\\s+(?<Number>\\d+))+");
         foreach (var line in lines)
@@ -21,8 +21,8 @@ public class Program
             var m = regex.Matches(line).First();
             if (m.Success)
             {
-                var cardNumber = m.Groups["CardNumber"].Value;
-
+                var cardNumber = int.Parse(m.Groups["CardNumber"].Value);
+                var nbOfCard = scratchCard[cardNumber - 1];
                 var winningNumber = m.Groups["WinningNumber"]
                     .Captures.Select(w => int.Parse(w.Value)).ToHashSet();
 
@@ -33,6 +33,12 @@ public class Program
 
                 if (wonNumbers.Any())
                 {
+                    int scratchedCardWon = cardNumber;
+                    for (int i = 0; i < wonNumbers.Count; i++)
+                    {
+                        scratchCard[scratchedCardWon] += nbOfCard;
+                        scratchedCardWon++;
+                    }
                     var cardValue = Math.Pow(2, wonNumbers.Count - 1);
                     Console.WriteLine($"Card {cardNumber} worth {cardValue}");
                     part1 += cardValue;
@@ -40,6 +46,8 @@ public class Program
             }
         }
 
+        var part2 = scratchCard.Sum();
         Console.WriteLine(part1);
+        Console.WriteLine(part2);
     }
 }
