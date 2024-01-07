b75af9cf0929541d89faacf2acf41818ec1b6518
FHocs <crono972@gmail.com>
2024-01-06 22:07:31 -0400

    advent of code 2023 - Exo 09

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,60 +1,27 @@
-var input = File.ReadAllLines(@"../../../../../2023/Exo08/input.txt");
+var input = File.ReadAllLines(@"../../../../../2023/Exo09/input.txt");
 var watch = System.Diagnostics.Stopwatch.StartNew();
-var instructions = input[0].Select(x => x == 'L' ? 0 : 1).ToArray();
-var nodes =
-    input.Skip(2)
-    .Select(x => x.Split(new[] { ' ', ',', '(', ')', '=' }, StringSplitOptions.RemoveEmptyEntries))
-    .ToDictionary(x => x[0], x => x[1..]);
 
-long result1 = 0;
-long result2 = 0;
+var values = input
+    .Select(s => s.Split(' ').Select(int.Parse).ToArray())
+    .ToArray();
+    
+Console.WriteLine(Part1(values));
+Console.WriteLine(Part2(values));
 
-{ // Part 1
-    for (var node = "AAA"; node != "ZZZ"; result1++)
-        node = nodes[node][instructions[result1 % instructions.Length]];
+static int Part1(int[][] values)
+{
+    var diffs = values.Select(GetDiffs).ToList();
+    return diffs
+        .Sum(aa => aa.Sum(a => a[^1]));
 }
 
-{ // Part2 (Utilizing loop frequency harmony)
-    var findloopFrequency = (string node) =>  // Scan until an end node is seen twice, first index is phase, index difference is period
-    {
-        var endSeen = new Dictionary<string, long>();
-        for (long i = 0; true; i++)
-        {
-            if (node[2] == 'Z')
-            {
-                if (endSeen.TryGetValue(node, out var lastSeen))
-                    return (phase: lastSeen, period: i - lastSeen);
-                else
-                    endSeen[node] = i;
-            }
-            node = nodes[node][instructions[i % instructions.Length]];
-        }
-    };
+static int Part2(int[][] values) =>
+    values.Select(GetDiffs)
+        .Sum(aa => aa.Reverse().Aggregate(0, (a, v) => v[0] - a));
 
-    var frequencies = 
-        nodes.Keys
-        .Where(x => x[2] == 'A')
-        .Select(x => findloopFrequency(x))
-        .ToList();
-
-    // Find harmony by moving harmony phase forward and increasing harmony period until it matches all frequencies
-    var harmonyPhase = frequencies[0].phase;
-    var harmonyPeriod = frequencies[0].period;
-    foreach (var freq in frequencies.Skip(1))
-    {
-        // Find new harmonyPhase by increasing phase in harmony period steps until harmony matches freq
-        for (; harmonyPhase < freq.phase || (harmonyPhase - freq.phase) % freq.period != 0; harmonyPhase += harmonyPeriod) ;
-
-        // Find the new harmonyPeriod by looking for the next position the harmony frequency matches freq (brute force least common multiplier)
-        long sample = harmonyPhase + harmonyPeriod;
-        for (; (sample - freq.phase) % freq.period != 0; sample += harmonyPeriod) ;
-        harmonyPeriod = sample - harmonyPhase;
-    }
-    result2 = harmonyPhase;
+static IEnumerable<int[]> GetDiffs(int[] a)
+{
+    yield return a;
+    while (a.Any(v => v != 0))
+        yield return a = a[1..].Select((v, i) => v - a[i]).ToArray();
 }
-
-watch.Stop();
-
-Console.WriteLine($"Result1 = {result1}");
-Console.WriteLine($"Result2 = {result2}");
-Console.WriteLine($"Runtime = {watch.ElapsedMilliseconds}ms");
