66592819d8f0f91c5c9941622bd8c8e338c4017b
FHocs <hocansungf@gmail.com>
2024-12-04 00:33:36 +0100

    Advent of code 2024 - Exo 02

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,42 +1,79 @@
-var lines = File.ReadAllLines(@"../../../../../2024/Exo01/input.txt");
+var lines = File.ReadAllLines(@"../../../../../2024/Exo02/sample.txt");
 
-var firstList = new List<int>();
-var secondList = new List<int>();
+Console.WriteLine(Part1(lines));
+Console.WriteLine(Part2(lines));
 
-foreach (var line in lines)
+static int Part1(string[] lines)
 {
-    var data = line.Split("   ");
-    firstList.Add(int.Parse(data[0]));
-    secondList.Add(int.Parse(data[1]));
-}
-
-    
-Console.WriteLine(Part1(firstList, secondList));
-Console.WriteLine(Part2(firstList, secondList));
-
-static int Part1(List<int> first, List<int> second)
-{
-    int distance = 0;
-    var orderedFirst = first.OrderBy(x => x).ToList();
-    var orderedSecond = second.OrderBy(x => x).ToList();
-    for (int i = 0; i < orderedFirst.Count; i++)
+    int safeReport = 0;
+    foreach (var line in lines)
     {
-        distance += Math.Abs(orderedFirst[i] - orderedSecond[i]);
+        var parsed = line.Split(' ').Select(int.Parse).ToArray();
+
+        // var goodAlgo = false;
+        // var pairs = parsed.Zip(parsed.Skip(1)).ToList();
+        // if (pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
+        //     pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3))
+        // {
+        //     goodAlgo = true;
+        // }
+        var lineIsSafe = true;
+        var direction = 0;
+        for (int i = 0; i <= parsed.Length - 2; i++)
+        {
+            var firstNum = parsed[i];
+            var secondNum = parsed[i + 1];
+            var currentSign = secondNum - firstNum == 0 ? 0 : secondNum - firstNum > 0 ? 1 : -1;
+            var difference = Math.Abs(secondNum - firstNum);
+            if (i == 0)
+            {
+                direction = currentSign;
+            }
+
+            lineIsSafe = lineIsSafe && currentSign == direction && currentSign != 0 && difference is <= 3 and >= 1;
+        }
+
+        // if (goodAlgo != lineIsSafe)
+        // {
+        //     Console.WriteLine(line );
+        // }
+        safeReport += lineIsSafe ? 1 : 0;
     }
 
-    return distance;
+    return safeReport;
 }
 
-static int Part2(List<int> first, List<int> second)
+static int Part2(string[] lines)
 {
-    var similarity = 0;
-    var occurenceSecondList = second.ToLookup(s => s);
-    for (int i = 0; i < first.Count; i++)
+    int safeReport = 0;
+    foreach (var line in lines)
     {
-        var current = first[i];
-        var simScore = occurenceSecondList.FirstOrDefault(kv => kv.Key == current)?.Count() ?? 0;
-        similarity += current * simScore;
+        var parsed = line.Split(' ').Select(int.Parse).ToArray();
+        var lineIsSafe = true;
+        var direction = 0;
+        for (int i = 0; i <= parsed.Length - 3; i++)
+        {
+            var firstNum = parsed[i];
+            var secondNum = parsed[i + 1];
+            var thirdNum = parsed[i + 2];
+                
+            var currentSign = secondNum - firstNum == 0 ? 0 : secondNum - firstNum > 0 ? 1 : -1;
+            var secondSign = thirdNum - firstNum ==0 ? 0 : thirdNum - firstNum > 0 ? 1 : -1;
+            var difference = Math.Abs(secondNum - firstNum);
+            var secondDifference = Math.Abs(thirdNum - firstNum);
+            if (i == 0)
+            {
+                direction = currentSign;
+            }
+
+            lineIsSafe = lineIsSafe && (
+                (currentSign == direction && currentSign != 0)
+                || (secondSign == direction && secondSign != 0)) && difference is <= 3 and >= 1;
+
+        }
+
+        safeReport += lineIsSafe ? 1 : 0;
     }
 
-    return similarity;
+    return safeReport;
 }
