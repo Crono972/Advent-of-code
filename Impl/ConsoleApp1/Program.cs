d3f1bea8a9a0bc2733870e179ba9c3b9bd6cde4f
Fhocs <hocansungf@gmail.com>
2024-12-04 17:29:07 +0100

    Advent of code 2024 - Exo03 - part 2

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,11 +1,25 @@
 using System.Text.RegularExpressions;
 
-var lines = File.ReadAllLines(@"../../../../../2024/Exo03/input.txt");
+Regex regex = new Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled);
 
+var lines = File.ReadAllLines(@"../../../../../2024/Exo03/input.txt");
 var line = string.Concat(lines);
 
-var regex = new Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled);
+Console.WriteLine(PartOne(line));
+Console.WriteLine(PartTwo(line));
 
-var matchCollection = regex.Matches(line);
-var result = matchCollection.Select(s => int.Parse(s.Groups[1].Value) * int.Parse(s.Groups[2].Value)).Sum();
-Console.WriteLine(result);
+int PartOne(string line)
+{
+    var matchCollection = regex.Matches(line);
+    var result = matchCollection.Select(s => int.Parse(s.Groups[1].Value) * int.Parse(s.Groups[2].Value)).Sum();
+    return result;
+}
+
+int PartTwo(string line)
+{
+    var splitFirst = line.Split("don't");
+    var activated = splitFirst.Skip(1).SelectMany(s => s.Split("do").Skip(1)).ToList();
+    activated.Add(splitFirst[0]);
+    var totalActivated = string.Concat(activated);
+    return PartOne(totalActivated);
+}
