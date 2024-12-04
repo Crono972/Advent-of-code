9671ee239882d15cf2c1c341663053a5a8633dc6
Fhocs <hocansungf@gmail.com>
2024-12-04 15:37:42 +0100

    Advent of codes 2024 - Exo 2 Part 2

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,7 +1,8 @@
-var lines = File.ReadAllLines(@"../../../../../2024/Exo02/sample.txt");
+var lines = File.ReadAllLines(@"../../../../../2024/Exo02/input.txt");
 
 Console.WriteLine(Part1(lines));
 Console.WriteLine(Part2(lines));
+Console.WriteLine(PartTwo(lines));
 
 static int Part1(string[] lines)
 {
@@ -56,9 +57,9 @@ static int Part2(string[] lines)
             var firstNum = parsed[i];
             var secondNum = parsed[i + 1];
             var thirdNum = parsed[i + 2];
-                
+
             var currentSign = secondNum - firstNum == 0 ? 0 : secondNum - firstNum > 0 ? 1 : -1;
-            var secondSign = thirdNum - firstNum ==0 ? 0 : thirdNum - firstNum > 0 ? 1 : -1;
+            var secondSign = thirdNum - firstNum == 0 ? 0 : thirdNum - firstNum > 0 ? 1 : -1;
             var difference = Math.Abs(secondNum - firstNum);
             var secondDifference = Math.Abs(thirdNum - firstNum);
             if (i == 0)
@@ -69,11 +70,40 @@ static int Part2(string[] lines)
             lineIsSafe = lineIsSafe && (
                 (currentSign == direction && currentSign != 0)
                 || (secondSign == direction && secondSign != 0)) && difference is <= 3 and >= 1;
-
         }
 
         safeReport += lineIsSafe ? 1 : 0;
     }
 
     return safeReport;
+}
+
+static object PartTwo(string[] dataLines)
+{
+    return dataLines.Select(s => s.Split(' ').Select(int.Parse).ToArray()).Count(samples => Attenuate(samples).Any(Valid));
+}
+//
+// public object PartTwo(string input) => 
+//     ParseSamples(input).Count(samples => Attenuate(samples).Any(Valid));
+
+IEnumerable<int[]> ParseSamples(string input) =>
+    input.Split("\n")
+        .Select(line => new { line, samples = line.Split(" ").Select(int.Parse) })
+        .Select(@t => @t.samples.ToArray());
+
+// Generates all possible variations of the input sequence by omitting 
+// either zero or one element from it.
+static IEnumerable<int[]> Attenuate(int[] samples) =>
+    from i in Enumerable.Range(0, samples.Length + 1)
+    let before = samples.Take(i - 1)
+    let after = samples.Skip(i)
+    select Enumerable.Concat(before, after).ToArray();
+
+// Checks the monothinicity condition by examining consecutive elements
+static bool Valid(int[] samples)
+{
+    var pairs = Enumerable.Zip(samples, samples.Skip(1));
+    return
+        pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
+        pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3);
 }
