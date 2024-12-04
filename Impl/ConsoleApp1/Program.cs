c9ac876eb5c617e827218ea04974bfcd37aa61a4
Fhocs <hocansungf@gmail.com>
2024-12-04 16:59:49 +0100

    Advent of code 2024 - Exo03 - part 1

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,109 +1,11 @@
-var lines = File.ReadAllLines(@"../../../../../2024/Exo02/input.txt");
+using System.Text.RegularExpressions;
 
-Console.WriteLine(Part1(lines));
-Console.WriteLine(Part2(lines));
-Console.WriteLine(PartTwo(lines));
+var lines = File.ReadAllLines(@"../../../../../2024/Exo03/input.txt");
 
-static int Part1(string[] lines)
-{
-    int safeReport = 0;
-    foreach (var line in lines)
-    {
-        var parsed = line.Split(' ').Select(int.Parse).ToArray();
+var line = string.Concat(lines);
 
-        // var goodAlgo = false;
-        // var pairs = parsed.Zip(parsed.Skip(1)).ToList();
-        // if (pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
-        //     pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3))
-        // {
-        //     goodAlgo = true;
-        // }
-        var lineIsSafe = true;
-        var direction = 0;
-        for (int i = 0; i <= parsed.Length - 2; i++)
-        {
-            var firstNum = parsed[i];
-            var secondNum = parsed[i + 1];
-            var currentSign = secondNum - firstNum == 0 ? 0 : secondNum - firstNum > 0 ? 1 : -1;
-            var difference = Math.Abs(secondNum - firstNum);
-            if (i == 0)
-            {
-                direction = currentSign;
-            }
+var regex = new Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled);
 
-            lineIsSafe = lineIsSafe && currentSign == direction && currentSign != 0 && difference is <= 3 and >= 1;
-        }
-
-        // if (goodAlgo != lineIsSafe)
-        // {
-        //     Console.WriteLine(line );
-        // }
-        safeReport += lineIsSafe ? 1 : 0;
-    }
-
-    return safeReport;
-}
-
-static int Part2(string[] lines)
-{
-    int safeReport = 0;
-    foreach (var line in lines)
-    {
-        var parsed = line.Split(' ').Select(int.Parse).ToArray();
-        var lineIsSafe = true;
-        var direction = 0;
-        for (int i = 0; i <= parsed.Length - 3; i++)
-        {
-            var firstNum = parsed[i];
-            var secondNum = parsed[i + 1];
-            var thirdNum = parsed[i + 2];
-
-            var currentSign = secondNum - firstNum == 0 ? 0 : secondNum - firstNum > 0 ? 1 : -1;
-            var secondSign = thirdNum - firstNum == 0 ? 0 : thirdNum - firstNum > 0 ? 1 : -1;
-            var difference = Math.Abs(secondNum - firstNum);
-            var secondDifference = Math.Abs(thirdNum - firstNum);
-            if (i == 0)
-            {
-                direction = currentSign;
-            }
-
-            lineIsSafe = lineIsSafe && (
-                (currentSign == direction && currentSign != 0)
-                || (secondSign == direction && secondSign != 0)) && difference is <= 3 and >= 1;
-        }
-
-        safeReport += lineIsSafe ? 1 : 0;
-    }
-
-    return safeReport;
-}
-
-static object PartTwo(string[] dataLines)
-{
-    return dataLines.Select(s => s.Split(' ').Select(int.Parse).ToArray()).Count(samples => Attenuate(samples).Any(Valid));
-}
-//
-// public object PartTwo(string input) => 
-//     ParseSamples(input).Count(samples => Attenuate(samples).Any(Valid));
-
-IEnumerable<int[]> ParseSamples(string input) =>
-    input.Split("\n")
-        .Select(line => new { line, samples = line.Split(" ").Select(int.Parse) })
-        .Select(@t => @t.samples.ToArray());
-
-// Generates all possible variations of the input sequence by omitting 
-// either zero or one element from it.
-static IEnumerable<int[]> Attenuate(int[] samples) =>
-    from i in Enumerable.Range(0, samples.Length + 1)
-    let before = samples.Take(i - 1)
-    let after = samples.Skip(i)
-    select Enumerable.Concat(before, after).ToArray();
-
-// Checks the monothinicity condition by examining consecutive elements
-static bool Valid(int[] samples)
-{
-    var pairs = Enumerable.Zip(samples, samples.Skip(1));
-    return
-        pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
-        pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3);
-}
+var matchCollection = regex.Matches(line);
+var result = matchCollection.Select(s => int.Parse(s.Groups[1].Value) * int.Parse(s.Groups[2].Value)).Sum();
+Console.WriteLine(result);
