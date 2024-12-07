76b22b3323af050489878066e382f051285acf51
Fhocs <hocansungf@gmail.com>
2024-12-07 09:08:44 +0100

    Advent of code 2024 - Exo05

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,63 +1,57 @@
-using System.Numerics;
-using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
-using System.Collections.Immutable;
-
-Complex Up = -Complex.ImaginaryOne;
-Complex Down = Complex.ImaginaryOne;
-Complex Left = -1;
-Complex Right = 1;
-
-var inputData = File.ReadAllText(@"../../../../../2024/Exo04/input.txt");
+var inputData = File.ReadAllLines(@"../../../../../2024/Exo05/input.txt");
 
 Console.WriteLine(PartOne(inputData));
 Console.WriteLine(PartTwo(inputData));
 
-object PartOne(string input) {
-    var mat = GetMap(input);
-    int count = 0;
-    foreach (Complex pt in mat.Keys)
-    foreach (var dir in new[] { Right, Right + Down, Down + Left, Down })
+(Comparer<string>, List<string[]>) ParseData(string[] input)
+{
+    var comparerData = new HashSet<(string, string)>();
+    var updates = new List<string[]>();
+    for (int i = 0; i < input.Length; i++)
     {
-        if (Matches(mat, pt, dir, "XMAS"))
+        var line = input[i];
+        if (string.IsNullOrWhiteSpace(line))
         {
-            count++;
+            continue;
+        }
+
+        if (line.Contains('|'))
+        {
+            var split = line.Split('|');
+            comparerData.Add((split[0], split[1]));
+        }
+        else
+        {
+            updates.Add(line.Split(",", StringSplitOptions.RemoveEmptyEntries));
         }
     }
 
-    return count;
+    var customComparer = Comparer<string>
+        .Create((a, b) => comparerData.Contains((a, b)) ? -1 : 1);
+    return (customComparer, updates);
 }
 
-object PartTwo(string input) {
-    var mat = GetMap(input);
-    int count = 0;
-    foreach (var pt in mat.Keys)
-    {
-        if (Matches(mat, pt + Up + Left, Down + Right, "MAS") && Matches(mat, pt + Down + Left, Up + Right, "MAS"))
-        {
-            count++;
-        }
-    }
-    return count;
+int PartOne(string[] input)
+{
+    var (customComparer, updates) = ParseData(input);
+    var validesUpdate = updates.Where(u => u.OrderBy(u => u, customComparer).SequenceEqual(u)).ToList();
+    var middleNumber = validesUpdate.Select(u => int.Parse(GetMiddle(u)));
+    return middleNumber.Sum();
 }
 
-// check if the pattern (or its reverse) can be read in the given direction 
-// starting from pt
-bool Matches(Map map, Complex pt, Complex dir, string pattern) {
-    var chars = Enumerable.Range(0, pattern.Length)
-        .Select(i => map.GetValueOrDefault(pt + i * dir))
-        .ToArray();
-    return
-        chars.SequenceEqual(pattern) ||
-        chars.SequenceEqual(pattern.Reverse());
+int PartTwo(string[] input)
+{
+    var (customComparer, updates) = ParseData(input);
+    var nonValidesUpdate = updates
+        .Where(u => !u.OrderBy(u => u, customComparer).SequenceEqual(u));
+    var partTwo = nonValidesUpdate
+        .Select(u => u.OrderBy(v => v, customComparer))
+        .Select(u => int.Parse(GetMiddle(u.ToArray()))).Sum();
+    return partTwo;
 }
 
-// store the points in a dictionary so that we can iterate over them and 
-// to easily deal with points outside the area using GetValueOrDefault
-static Map GetMap(string input) {
-    var map = input.Split("\n");
-    return (
-        from y in Enumerable.Range(0, map.Length)
-        from x in Enumerable.Range(0, map[0].Length)
-        select new KeyValuePair<Complex, char>(Complex.ImaginaryOne * y + x, map[y][x])
-    ).ToImmutableDictionary();
+string GetMiddle(string[] arr)
+{
+    var middleIdx = arr.Length / 2;
+    return arr[middleIdx];
 }
