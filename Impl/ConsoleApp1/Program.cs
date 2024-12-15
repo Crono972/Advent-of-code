c91283ecd04ab475c0159a56828ed7f1a4a8691d
FHocs <hocansungf@gmail.com>
2024-12-15 14:59:46 +0100

    Advent of code 2024 - Exo14

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,41 +1,96 @@
-using System.Text.RegularExpressions;
-using Machine = (Vec2 a, Vec2 b, Vec2 p);
+using System.Numerics;
 
-var lines = File.ReadAllText(@"../../../../../2024/Exo13/sample.txt");
+var lines = File.ReadAllLines(@"../../../../../2024/Exo14/input.txt");
+var mapWidth = 101;
+var mapHeight = 103;
+
 Console.WriteLine(PartOne(lines));
 Console.WriteLine(PartTwo(lines));
 
-long PartOne(string input) => Parse(input).Sum(GetPrize);
-long PartTwo(string input) => Parse(input, shift: 10000000000000).Sum(GetPrize);
-
-long GetPrize(Machine m) {
-    var (a, b, p) = m;
-
-    // solve a * i + b * j = p for i and j using Cramer's rule
-    var i = Det(p, b) / Det(a, b);
-    var j = Det(a, p) / Det(a, b);
-
-    // return the prize when a non negative _integer_ solution is found
-    if (i >= 0 && j >= 0 && a.x * i + b.x * j == p.x && a.y * i + b.y * j == p.y) {
-        return 3 * i + j;
-    } else {
-        return 0;
-    }
-}
-long Det(Vec2 a, Vec2 b) => a.x * b.y - a.y * b.x;
-
-IEnumerable<Machine> Parse(string input, long shift=0) {
-    var blocks = input.Split("\r\n\r\n");
-    foreach (var block in blocks) {
-        var nums =
-            Regex.Matches(block, @"\d+", RegexOptions.Multiline)
-                .Select(m => int.Parse(m.Value))
-                .Chunk(2).Select(p => new Vec2(p[0], p[1]))
-                .ToArray();
-
-        nums[2] = new Vec2(nums[2].x + shift, nums[2].y + shift);
-        yield return (nums[0], nums[1], nums[2]);
-    }
+long PartOne(string[] input)
+{
+    var data = Parse(input);
+    var endTurnPosition = data.Select(r => GetPositionAfterNSeconds(r.position, r.vector, 100)).ToList();
+    var endTurnByQuadrant = endTurnPosition.ToLookup(p => QuadrantPosition(p, mapWidth, mapHeight));
+    return endTurnByQuadrant.Where(kv => kv.Key != "invalid").Aggregate(1, (acc, grouping) => acc * grouping.Count());
 }
 
-record struct Vec2(long x, long y);
+long PartTwo(string[] input)
+{
+    var data = Parse(input);
+    var numberOfPts = data.Count;
+    var turn = 0;
+    var diffPoint = data.Select(r => r.position).ToHashSet();
+    while(diffPoint.Count != numberOfPts)
+    {
+        turn++;
+        data = data.Select(r => (GetPositionAfterNSeconds(r.position, r.vector, 1), r.vector)).ToList();
+        diffPoint = data.Select(r => r.position).ToHashSet();
+    }
+
+    for (int y = 0; y < mapHeight; y++)
+    {
+        for (int x = 0; x < mapWidth; x++)
+        {
+            Console.Write(data.Any(r => r.position == x + y * Complex.ImaginaryOne) ? "#" : ".");
+        }
+        Console.WriteLine();
+    }
+    
+    return turn;
+}
+
+Complex GetPositionAfterNSeconds(Complex position, Complex vector, int seconds)
+{
+    var endPosition = position;
+    for (int i = 0; i < seconds; i++)
+    {
+        endPosition = ((endPosition.Real + vector.Real + mapWidth) % mapWidth) + ((endPosition.Imaginary + vector.Imaginary + mapHeight) % mapHeight) * Complex.ImaginaryOne;
+    }
+    return endPosition;
+}
+
+string QuadrantPosition(Complex position, int mapWide, int mapHeight)
+{
+    var halfWide = mapWide / 2;
+    var halfHeight = mapHeight / 2;
+    if(position.Real < halfWide)
+    {
+        if (position.Imaginary < halfHeight)
+        {
+            return "top-left";
+        } 
+        if(position.Imaginary > halfHeight)
+        {
+            return "bottom-left";
+        }
+    }
+    if(position.Real > halfWide)
+    {
+        if (position.Imaginary < halfHeight)
+        {
+            return "top-right";
+        } 
+        if(position.Imaginary > halfHeight)
+        {
+            return "bottom-right";
+        }
+    }
+    
+    return "invalid";
+}
+
+IList<(Complex position, Complex vector)> Parse(string[] input)
+{
+    var result = new List<(Complex position, Complex vector)>();
+    foreach (var line in input)
+    {
+        var parts = line.Split(" ");
+        var firstPart = parts[0].Replace("p=", "").Split(",");
+        var position = new Complex(int.Parse(firstPart[0]), int.Parse(firstPart[1]));
+        var secondPart = parts[1].Replace("v=", "").Split(",");
+        var vector = new Complex(int.Parse(secondPart[0]), int.Parse(secondPart[1]));
+        result.Add((position, vector));
+    }
+    return result;
+}
