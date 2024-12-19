ed6e3d9dce263eb6f5debea4b4726d55869b0fee
FHocs <hocansungf@gmail.com>
2024-12-19 18:57:08 +0100

    Advent of code 2024 - Exo18

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -6,183 +6,71 @@ using State = (System.Numerics.Complex pos, System.Numerics.Complex dir);
 
 internal class Program
 {
-    enum Opcode
+    static object PartOne(string[] input)
     {
-        Adv,
-        Bxl,
-        Bst,
-        Jnz,
-        Bxc,
-        Out,
-        Bdv,
-        Cdv
+        var data = GetBlocks(input).Take(1024).ToList();
+        return Distance(data);
     }
 
-    static string Run(string input)
-    {
-        var (state, program) = Parse(input);
-        var combo = (long op) => op < 4 ? (int)op : (int)state[op - 4];
-        var res = new List<int>();
-        for (var ip = 0; ip < program.Length; ip += 2)
-        {
-            switch ((Opcode)program[ip], program[ip + 1])
-            {
-                case (Opcode.Adv, var op): state[0] = state[0] >> combo(op); break;
-                case (Opcode.Bdv, var op): state[1] = state[0] >> combo(op); break;
-                case (Opcode.Cdv, var op): state[2] = state[0] >> combo(op); break;
-                case (Opcode.Bxl, var op): state[1] = state[1] ^ op; break;
-                case (Opcode.Bst, var op): state[1] = combo(op) % 8; break;
-                case (Opcode.Jnz, var op): ip = state[0] == 0 ? ip : (int)op - 2; break;
-                case (Opcode.Bxc, var op): state[1] = state[1] ^ state[2]; break;
-                case (Opcode.Out, var op): res.Add(combo(op) % 8); break;
+    static object PartTwo(string[] input) {
+        // find the first block position that will cut off the goal position
+        // we can use a binary search for this
+
+        var blocks = GetBlocks(input);
+        var (lo, hi) = (0, blocks.Length);
+        while (hi - lo > 1) {
+            var m = (lo + hi) / 2;
+            if (Distance(blocks.Take(m).ToList()) == null) {
+                hi = m;
+            } else {
+                lo = m;
             }
         }
-
-        return string.Join(",", res);
+        return $"{blocks[lo].Real},{blocks[lo].Imaginary}";
     }
 
-    /*
-        Determines register A for the given output. The search works recursively and in reverse order,
-        starting from the last number to be printed and ending with the first.
-    */
-    static IEnumerable<long> GenerateA(List<long> output)
-    {
-        if (!output.Any())
-        {
-            yield return 0;
-            yield break;
-        }
+    static int? Distance(IList<Complex> blocks) {
+        // our standard priority queue based path finding
+        
+        var size = 70;
+        var (start, goal) = (0, size + size * Complex.ImaginaryOne);
+        var blocked = blocks.Concat([start]).ToHashSet();
+    
+        var q = new PriorityQueue<Complex, int>();
+        q.Enqueue(start, 0);
+        while (q.TryDequeue(out var pos, out var dist)) {
+            if (pos == goal) {
+                return dist;
+            } 
 
-        // this loop is pretty much the assembly code of the program reimplemented in c#
-        foreach (var ah in GenerateA(output[1..]))
-        {
-            for (var al = 0; al < 8; al++)
-            {
-                var a = ah * 8 + al;
-                long b = a % 8;
-                b = b ^ 3;
-                var c = a >> (int)b;
-                b = b ^ c;
-                b = b ^ 5;
-                if (output[0] == b % 8)
-                {
-                    yield return a;
+            foreach (var dir in new[] { 1, -1, Complex.ImaginaryOne, -Complex.ImaginaryOne }) {
+                var posT = pos + dir;
+                if (!blocked.Contains(posT) &&
+                    0 <= posT.Imaginary && posT.Imaginary <= size &&
+                    0 <= posT.Real && posT.Real <= size
+                   ) {
+                    q.Enqueue(posT, dist + 1);
+                    blocked.Add(posT);
                 }
             }
         }
+        return null;
     }
 
-    static List<long> CursedDfs(List<long> operands, long curVal, int depth)
+    static Complex[] GetBlocks(string[] input)
     {
-        List<long> res = new();
-        if (depth > operands.Count) return res;
-        var tmp = curVal << 3;
-        for (int i = 0; i < 8; i++)
-        {
-            var tmpRes = RunProgram(tmp + i, operands);
-            if (tmpRes.SequenceEqual(operands.TakeLast(depth + 1)))
+        return input.Where(l => !string.IsNullOrWhiteSpace(l))
+            .Select(l => Regex.Matches(l, @"\d+"))
+            .Select(m =>
             {
-                if (depth + 1 == operands.Count) res.Add(tmp + i);
-                res.AddRange(CursedDfs(operands, tmp + i, depth + 1));
-            }
-        }
-
-        return res;
+                
+                return int.Parse(m[0].Value) + int.Parse(m[1].Value) * Complex.ImaginaryOne;
+            }).ToArray();
     }
-
-    static List<long> RunProgram(long regA, List<long> operands)
-    {
-        long regB = 0;
-        long regC = 0;
-        List<long> output = new();
-        int pc = 0;
-        while (pc < operands.Count)
-        {
-            long combo = (operands[pc + 1]) switch
-            {
-                0 => 0,
-                1 => 1,
-                2 => 2,
-                3 => 3,
-                4 => regA,
-                5 => regB,
-                6 => regC,
-                _ => long.MinValue
-            };
-
-            long literal = operands[pc + 1];
-            long res = 0;
-            bool jumped = false;
-            switch (operands[pc])
-            {
-                case 0:
-                    res = (long)(regA / Math.Pow(2, combo));
-                    regA = res;
-                    break;
-                case 1:
-                    res = regB ^ literal;
-                    regB = res;
-                    break;
-                case 2:
-                    res = combo % 8;
-                    regB = res;
-                    break;
-                case 3:
-                    if (regA != 0)
-                    {
-                        pc = (int)literal;
-                        jumped = true;
-                    }
-
-                    break;
-                case 4:
-                    res = regB ^ regC;
-                    regB = res;
-                    break;
-                case 5:
-                    output.Add(combo % 8);
-                    break;
-                case 6:
-                    res = (long)(regA / Math.Pow(2, combo));
-                    regB = res;
-                    break;
-                case 7:
-                    res = (long)(regA / Math.Pow(2, combo));
-                    regC = res;
-                    break;
-                default: break;
-            }
-
-            if (!jumped) pc += 2;
-            if (output.Count > operands.Count) break;
-        }
-
-        return output;
-    }
-
-    static (long[] state, long[] program) Parse(string input)
-    {
-        var blocks = input.Replace("\r\n", "\n").Split("\n\n").Select(ParseNums).ToArray();
-        return (blocks[0], blocks[1]);
-    }
-
-    static long[] ParseNums(string st) =>
-        Regex.Matches(st, @"\d+", RegexOptions.Multiline)
-            .Select(m => long.Parse(m.Value))
-            .ToArray();
-
-    static object PartOne(string input) => Run(input);
-    // static object PartTwo(string input) => GenerateA(Parse(input).program.ToList()).Min();
-
-    static long PartTwo(string input)
-    {
-        var (state, program) = Parse(input);
-        return CursedDfs(program.ToList(), 0, 0).Min();
-    }
-
+        
     public static void Main(string[] args)
     {
-        var lines = File.ReadAllText(@"../../../../../2024/Exo17/input.txt");
+        var lines = File.ReadLines(@"../../../../../2024/Exo18/input.txt").ToArray();
         Console.WriteLine(PartOne(lines));
         Console.WriteLine(PartTwo(lines));
     }
