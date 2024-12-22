2e57b44550f71ae968feed06fa94d692492e9d57
FHocs <hocansungf@gmail.com>
2024-12-22 13:23:03 +0100

    Advent of code 2024 - Exo20

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -7,35 +7,60 @@ using State = (System.Numerics.Complex pos, System.Numerics.Complex dir);
 
 internal class Program
 {
-    static object PartOne(string[] input) => MatchCounts(input).Count(c => c != 0);
+    static object PartOne(string[] input) => Solve(input, 2);
 
-    static object PartTwo(string[] input) => MatchCounts(input).Sum();
+    static object PartTwo(string[] input) => Solve(input, 20);
 
-    static IEnumerable<long> MatchCounts(string[] input) {
-        var blocks = input[2..];
-        var towels = input[0].Split(", ");
-        return 
-            from pattern in  blocks 
-            select MatchCount(towels, pattern, new ConcurrentDictionary<string, long>());
+    static int Solve(string[] input, int cheat) {
+        var path = GetPath(input);
+        var indices = Enumerable.Range(0, path.Length).ToArray();
+
+        // sum up the worthy cheats for each index along the path
+        var cheatsFromI = (int i) => (
+            from j in indices[0..i]
+            let dist = Manhattan(path[i], path[j])
+            let saving = i - (j + dist) 
+            where dist <= cheat && saving >= 100
+            select 1
+        ).Sum();
+
+        // parallel is gold today, it gives us an 3-4x boost
+        return indices.AsParallel().Select(cheatsFromI).Sum();
     }
 
-    // computes the number of ways the pattern can be build up from the towels. 
-    // works recursively by matching the prefix of the pattern with each towel.
-    // a full match is found when the pattern becomes empty. the cache is applied 
-    // to _drammatically_ speed up execution
-    static long MatchCount(string[] towels, string pattern, ConcurrentDictionary<string, long> cache) =>
-        cache.GetOrAdd(pattern, (pattern) => 
-            pattern switch {
-                "" => 1,
-                _  =>  towels
-                    .Where(pattern.StartsWith)
-                    .Sum(towel => MatchCount(towels, pattern[towel.Length ..], cache))
-            }
-        );
-        
+    static int Manhattan(Complex a, Complex b) =>
+        (int)(Math.Abs(a.Imaginary - b.Imaginary) + Math.Abs(a.Real - b.Real));
+
+    // Follow the path from finish to start, supposed that there is a single track in the input.
+    // The index of a position in the returned array equals to its distance from the finish
+    static Complex[] GetPath(string[] input) {
+        var map = (
+            from y in Enumerable.Range(0, input.Length)
+            from x in Enumerable.Range(0, input[0].Length)
+            select new KeyValuePair<Complex, char>(x + y * Complex.ImaginaryOne, input[y][x])
+        ).ToDictionary();
+
+        Complex[] dirs = [-1, 1, Complex.ImaginaryOne, -Complex.ImaginaryOne];
+
+        var start = map.Keys.Single(k => map[k] == 'S');
+        var goal = map.Keys.Single(k => map[k] == 'E');
+
+        var prev = (Complex?)null;
+        var cur = goal;
+        var res = new List<Complex> { cur };
+
+        while (cur != start) {
+            var dir = dirs.Single(dir => map[cur + dir] != '#' && cur + dir != prev);
+            prev = cur;
+            cur += dir;
+            // (prev, cur) = (cur, cur + dir);
+            res.Add(cur);
+        }
+        return res.ToArray();
+    }
     public static void Main(string[] args)
     {
-        var lines = File.ReadLines(@"../../../../../2024/Exo19/input.txt").ToArray();
+        var lines = File.ReadLines(@"../../../../../2024/Exo20/input.txt").ToArray();
         Console.WriteLine(PartOne(lines));
         Console.WriteLine(PartTwo(lines));
     }
