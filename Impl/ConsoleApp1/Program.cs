ffe0646a2dd29454886fe1db6252dd18bdeb14cb
FHocs <hocansungf@gmail.com>
2024-12-27 09:48:12 +0100

    Advent of code 2024 - Exo24

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,60 +1,118 @@
-using System.Collections.Frozen;
+using System.Text.RegularExpressions;
+using Circuit = System.Collections.Generic.Dictionary<string, Gate>;
 
-var lines = File.ReadAllText(@"../../../../../2024/Exo23/input.txt");
+var lines = File.ReadAllText(@"../../../../../2024/Exo24/input.txt");
 
-Console.WriteLine($"PartOne : {PartOne(lines)}");
-Console.WriteLine($"PartTwo : {PartTwo(lines)}");
+Console.WriteLine($"PartOne : {PartOne(lines.Replace("\r\n", "\n"))}");
+Console.WriteLine($"PartTwo : {PartTwo(lines.Replace("\r\n", "\n"))}");
 
-long PartOne(string input)
+
+static object PartOne(string input)
 {
-    var g = GetGraph(input);
-    var components = GetSeed(g);
-    components = Grow(g, components);
-    components = Grow(g, components);
-    return components.Count(c => Members(c).Any(m => m.StartsWith("t")));
-}
+    var (inputs, circuit) = Parse(input);
 
-object PartTwo(string input)
-{
-    var g = GetGraph(input);
-    var components = GetSeed(g);
-    while (components.Count > 1)
+    var outputs = from label in circuit.Keys where label.StartsWith("z") select label;
+
+    var res = 0L;
+    foreach (var label in outputs.OrderByDescending(label => label))
     {
-        components = Grow(g, components);
+        res = res * 2 + Evaluate(label, circuit, inputs);
     }
 
-    return components.Single();
+    return res;
 }
 
-HashSet<string> GetSeed(Dictionary<string, HashSet<string>> g) => g.Keys.ToHashSet();
-
-HashSet<string> Grow(Dictionary<string, HashSet<string>> g, HashSet<string> components) => (
-    from c in components.AsParallel()
-    let members = Members(c)
-    from neighbour in members.SelectMany(m => g[m]).Distinct()
-    where !members.Contains(neighbour)
-    where members.All(m => g[neighbour].Contains(m))
-    select Extend(c, neighbour)
-).ToHashSet();
-
-IEnumerable<string> Members(string c) =>
-    c.Split(",");
-
-string Extend(string c, string item) =>
-    string.Join(",", Members(c).Append(item).OrderBy(x => x));
-
-Dictionary<string, HashSet<string>> GetGraph(string input)
+static int Evaluate(string label, Circuit circuit, Dictionary<string, int> inputs)
 {
-    var edges =
-        from line in input.Replace("\r\n", "\n").Split("\n")
-        let nodes = line.Split("-")
-        from edge in new[] { (nodes[0], nodes[1]), (nodes[1], nodes[0]) }
-        select (From: edge.Item1, To: edge.Item2);
-
-    return (
-        from e in edges
-        group e by e.From
-        into g
-        select (g.Key, g.Select(e => e.To).ToHashSet())
-    ).ToDictionary();
+    if (inputs.TryGetValue(label, out var res))
+    {
+        return res;
+    }
+    else
+    {
+        return circuit[label] switch
+        {
+            Gate(var in1, "AND", var in2) => Evaluate(in1, circuit, inputs) & Evaluate(in2, circuit, inputs),
+            Gate(var in1, "OR", var in2) => Evaluate(in1, circuit, inputs) | Evaluate(in2, circuit, inputs),
+            Gate(var in1, "XOR", var in2) => Evaluate(in1, circuit, inputs) ^ Evaluate(in2, circuit, inputs),
+            _ => throw new Exception(circuit[label].ToString()),
+        };
+    }
 }
+
+static object PartTwo(string input)
+{
+    var circuit = Parse(input).circuit;
+    return string.Join(",", Fix(circuit).OrderBy(label => label));
+}
+
+// the circuit should define a full adder for two 44 bit numbers
+// this fixer is specific to my input.
+static IEnumerable<string> Fix(Circuit circuit)
+{
+    var cin = Output(circuit, "x00", "AND", "y00");
+    for (var i = 1; i < 45; i++)
+    {
+        var x = $"x{i:D2}";
+        var y = $"y{i:D2}";
+        var z = $"z{i:D2}";
+
+        var xor1 = Output(circuit, x, "XOR", y);
+        var and1 = Output(circuit, x, "AND", y);
+        var xor2 = Output(circuit, cin, "XOR", xor1);
+        var and2 = Output(circuit, cin, "AND", xor1);
+
+        if (xor2 == null && and2 == null)
+        {
+            return SwapAndFix(circuit, xor1, and1);
+        }
+
+        var carry = Output(circuit, and1, "OR", and2);
+        if (xor2 != z)
+        {
+            return SwapAndFix(circuit, z, xor2);
+        }
+        else
+        {
+            cin = carry;
+        }
+    }
+
+    return [];
+}
+
+static IEnumerable<string> SwapAndFix(Circuit circuit, string out1, string out2)
+{
+    (circuit[out1], circuit[out2]) = (circuit[out2], circuit[out1]);
+    return Fix(circuit).Concat([out1, out2]);
+}
+
+static string Output(Circuit circuit, string x, string kind, string y) =>
+    circuit.SingleOrDefault(pair =>
+        (pair.Value.in1 == x && pair.Value.kind == kind && pair.Value.in2 == y) ||
+        (pair.Value.in1 == y && pair.Value.kind == kind && pair.Value.in2 == x)
+    ).Key;
+
+static (Dictionary<string, int> inputs, Circuit circuit) Parse(string input)
+{
+    var inputs = new Dictionary<string, int>();
+    var circuit = new Circuit();
+
+    var blocks = input.Split("\n\n");
+
+    foreach (var line in blocks[0].Split("\n"))
+    {
+        var parts = line.Split(": ");
+        inputs.Add(parts[0], int.Parse(parts[1]));
+    }
+
+    foreach (var line in blocks[1].Split("\n"))
+    {
+        var parts = Regex.Matches(line, "[a-zA-z0-9]+").Select(m => m.Value).ToArray();
+        circuit.Add(parts[3], new Gate(in1: parts[0], kind: parts[1], in2: parts[2]));
+    }
+
+    return (inputs, circuit);
+}
+
+record Gate(string in1, string kind, string in2);
