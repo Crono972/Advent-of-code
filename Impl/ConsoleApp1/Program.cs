0b334ee67597878e62999049520bb4eb33b51e1f
FHocs <hocansungf@gmail.com>
2024-12-27 09:48:34 +0100

    Advent of code 2024 - Exo25

--- Impl/ConsoleApp1/Program.cs
+++ Impl/ConsoleApp1/Program.cs
@@ -1,118 +1,23 @@
 using System.Text.RegularExpressions;
-using Circuit = System.Collections.Generic.Dictionary<string, Gate>;
 
-var lines = File.ReadAllText(@"../../../../../2024/Exo24/input.txt");
+var lines = File.ReadAllText(@"../../../../../2024/Exo25/input.txt");
 
 Console.WriteLine($"PartOne : {PartOne(lines.Replace("\r\n", "\n"))}");
-Console.WriteLine($"PartTwo : {PartTwo(lines.Replace("\r\n", "\n"))}");
+// Console.WriteLine($"PartTwo : {PartTwo(lines.Replace("\r\n", "\n"))}");
 
 
-static object PartOne(string input)
-{
-    var (inputs, circuit) = Parse(input);
+static object PartOne(string input) {
+    var patterns = input.Split("\n\n").Select(b => b.Split("\n")).ToList();
+    var keys = patterns.Where(p => p[0][0] == '.').Select(ParsePattern).ToList();
+    var locks = patterns.Where(p => p[0][0] == '#').Select(ParsePattern).ToList();
 
-    var outputs = from label in circuit.Keys where label.StartsWith("z") select label;
+    return keys.Sum(k => locks.Count(l => Match(l, k)));
 
-    var res = 0L;
-    foreach (var label in outputs.OrderByDescending(label => label))
-    {
-        res = res * 2 + Evaluate(label, circuit, inputs);
-    }
+    bool Match(int[] k, int[] l) =>
+        Enumerable.Range(0, k.Length).All(i => k[i] + l[i] <= 7);
 
-    return res;
+    int[] ParsePattern(string[] lines) =>
+        Enumerable.Range(0, lines[0].Length).Select(x =>
+            Enumerable.Range(0, lines.Length).Count(y => lines[y][x] == '#')
+        ).ToArray();
 }
-
-static int Evaluate(string label, Circuit circuit, Dictionary<string, int> inputs)
-{
-    if (inputs.TryGetValue(label, out var res))
-    {
-        return res;
-    }
-    else
-    {
-        return circuit[label] switch
-        {
-            Gate(var in1, "AND", var in2) => Evaluate(in1, circuit, inputs) & Evaluate(in2, circuit, inputs),
-            Gate(var in1, "OR", var in2) => Evaluate(in1, circuit, inputs) | Evaluate(in2, circuit, inputs),
-            Gate(var in1, "XOR", var in2) => Evaluate(in1, circuit, inputs) ^ Evaluate(in2, circuit, inputs),
-            _ => throw new Exception(circuit[label].ToString()),
-        };
-    }
-}
-
-static object PartTwo(string input)
-{
-    var circuit = Parse(input).circuit;
-    return string.Join(",", Fix(circuit).OrderBy(label => label));
-}
-
-// the circuit should define a full adder for two 44 bit numbers
-// this fixer is specific to my input.
-static IEnumerable<string> Fix(Circuit circuit)
-{
-    var cin = Output(circuit, "x00", "AND", "y00");
-    for (var i = 1; i < 45; i++)
-    {
-        var x = $"x{i:D2}";
-        var y = $"y{i:D2}";
-        var z = $"z{i:D2}";
-
-        var xor1 = Output(circuit, x, "XOR", y);
-        var and1 = Output(circuit, x, "AND", y);
-        var xor2 = Output(circuit, cin, "XOR", xor1);
-        var and2 = Output(circuit, cin, "AND", xor1);
-
-        if (xor2 == null && and2 == null)
-        {
-            return SwapAndFix(circuit, xor1, and1);
-        }
-
-        var carry = Output(circuit, and1, "OR", and2);
-        if (xor2 != z)
-        {
-            return SwapAndFix(circuit, z, xor2);
-        }
-        else
-        {
-            cin = carry;
-        }
-    }
-
-    return [];
-}
-
-static IEnumerable<string> SwapAndFix(Circuit circuit, string out1, string out2)
-{
-    (circuit[out1], circuit[out2]) = (circuit[out2], circuit[out1]);
-    return Fix(circuit).Concat([out1, out2]);
-}
-
-static string Output(Circuit circuit, string x, string kind, string y) =>
-    circuit.SingleOrDefault(pair =>
-        (pair.Value.in1 == x && pair.Value.kind == kind && pair.Value.in2 == y) ||
-        (pair.Value.in1 == y && pair.Value.kind == kind && pair.Value.in2 == x)
-    ).Key;
-
-static (Dictionary<string, int> inputs, Circuit circuit) Parse(string input)
-{
-    var inputs = new Dictionary<string, int>();
-    var circuit = new Circuit();
-
-    var blocks = input.Split("\n\n");
-
-    foreach (var line in blocks[0].Split("\n"))
-    {
-        var parts = line.Split(": ");
-        inputs.Add(parts[0], int.Parse(parts[1]));
-    }
-
-    foreach (var line in blocks[1].Split("\n"))
-    {
-        var parts = Regex.Matches(line, "[a-zA-z0-9]+").Select(m => m.Value).ToArray();
-        circuit.Add(parts[3], new Gate(in1: parts[0], kind: parts[1], in2: parts[2]));
-    }
-
-    return (inputs, circuit);
-}
-
-record Gate(string in1, string kind, string in2);
